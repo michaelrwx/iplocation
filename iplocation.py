b39abbfa0bc8f1e1ebcca953c80e878856fe0639
Michael Simonelli <42250312+michaelrwx@users.noreply.github.com>
2024-03-23 09:32:34 -0400

    Add files via upload

--- iplocation.py
+++ iplocation.py
@@ -1,37 +1,25 @@
-#This program was made to teach me how to use packages from pip. I wanted to make something like nslookup or traceroute except it gives out the IP right away!
 import ipinfo
-
+import socket
+import sys
+ 
 #Define input variable
-print(f"Enter an IP address: ")
-givenip = input()
+if len(sys.argv) != 2:
+    print("Usage: iplocation <IP ADDRESS> or <WEBSITE NAME>")
+    sys.exit(1)
 
-#Variable definitions that get the ipinfo package to work
-handler = ipinfo.getHandler()
-details = handler.getDetails(givenip)
-
-#Print statements with normal programs
-print(f"You entered {givenip}.")
-print(f"Your ISP is {details.org}.")
-print(f"In fact your city is {details.city}, {details.region}. Located in the beautiful land of {details.country_name}") 
-print(f"Your coordinates are {details.loc}, latitude-longitude order of course.")
-print(f"I believe your hostname is {details.hostname}.")
-
-print(f"Would you like a detailed view? [y/n]")
-decision = input()
-
-if decision == "y" or decision == "Y":
-    print(details.all) #Maybe make this look cleaner
-else:
-    print("Peace") 
-
-#To Do: 
-    #Give option to parse results to a file
-    #Create DNS compatibility
-
-
-
-
-
-
-
-
+try: 
+    givenip = sys.argv[1]
+ 
+    #Variable definitions that get the ipinfo package to work
+    handler = ipinfo.getHandler()
+    details = handler.getDetails(socket.gethostbyname(givenip)) #the socket.gethostbyname converts a DNS line into an IP
+ 
+    #Print statements with normal programs
+    print(f"IP entered: {givenip}.")
+    print(f"ISP: {details.org}.")
+    print(f"Location: {details.city}, {details.region}, {details.country}")
+    print(f"Coordinates {details.loc}")
+    print(f"IP Hostname: {details.hostname}.")
+except:
+    print("Invalid input. Try again (EX: 8.8.8.8 or www.google.com)")
+    
